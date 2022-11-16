553c0cf902f5255cab4e5f114224232b8b224fba
Robert0321 <shat0806449@gmail.com>
2022-11-16 16:41:50 +0800

    Initial Commit

---
+++ Scrollview/ScrollviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollviewApp.swift
+//  Scrollview
+//
+//  Created by robert on 2022/11/16.
+//
+
+import SwiftUI
+
+@main
+struct ScrollviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
